7f48cb5ed5e860022ece2f911394d9cd5a6c8645
Arnab Sen <arnb.sen@hotmail.com>
2018-06-27 09:48:18 +0530

    Commit

--- src/main/java/com/thinkxfactor/zomatoplus/repository/RestaurantRepository.java
+++ src/main/java/com/thinkxfactor/zomatoplus/repository/RestaurantRepository.java
@@ -1,5 +1,7 @@
 package com.thinkxfactor.zomatoplus.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.thinkxfactor.zomatoplus.models.Restaurant;
@@ -8,4 +10,8 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 	
 	public Restaurant findByName(String s);
 	public Restaurant findById(long l);
+	public List<Restaurant> findAllByCity(String s);
+	public Restaurant findAllByCityAndName(String s1, String s2);
+	
+
 }
